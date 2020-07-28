20101d50d8010d9fa28e1399b334d60eb385f2d9
Felix Nagel <felixnagel105@gmail.com>
2020-07-28 10:45:02 +0200

    Updates

---
+++ precache-manifest.72200c20203f90bfb8688f00311123e3.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e4affdb0f72e7145139dc41dffbcabd",
+    "url": "/yahztee/index.html"
+  },
+  {
+    "revision": "74ab1ab3e691020230d1",
+    "url": "/yahztee/static/css/2.71acd7af.chunk.css"
+  },
+  {
+    "revision": "12c9a5ec8874849ca0c0",
+    "url": "/yahztee/static/css/main.8f86418a.chunk.css"
+  },
+  {
+    "revision": "74ab1ab3e691020230d1",
+    "url": "/yahztee/static/js/2.9f032e77.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yahztee/static/js/2.9f032e77.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12c9a5ec8874849ca0c0",
+    "url": "/yahztee/static/js/main.68e8e510.chunk.js"
+  },
+  {
+    "revision": "52c70d851ddb9d870ad4",
+    "url": "/yahztee/static/js/runtime-main.59bb50fb.js"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/yahztee/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/yahztee/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/yahztee/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/yahztee/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/yahztee/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/yahztee/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/yahztee/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/yahztee/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/yahztee/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/yahztee/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/yahztee/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/yahztee/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/yahztee/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/yahztee/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/yahztee/static/media/fa-solid-900.ee09ad75.woff"
+  }
+]);
